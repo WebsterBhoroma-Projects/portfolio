77822af9ab0bf4fd8d947c6062cf5fccd7630f3d
WebsterBhoroma-Projects <websterbhoroma93@gmail.com>
2026-02-09 21:45:35 +0200

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,41 @@
+
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Awards from './components/Awards';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        <section id="home">
+          <Hero />
+        </section>
+        <section id="about">
+          <About />
+        </section>
+        <section id="experience">
+          <Experience />
+        </section>
+        <section id="projects">
+          <Projects />
+        </section>
+        <section id="awards">
+          <Awards />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
